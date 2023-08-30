f3eafbfe81a8079e59e1b1fa32bf9a1be2513a49
Juan Pablo Gomez Martinez <juanpablo.gomezmartinez89@gmail.com>
2023-08-29 20:51:17 -0600

    Agregando componente ImageSlider
    este componente es para que imágenes se vayan cambiando.

--- webpage/src/components/ImageSlider/ImageSlider.styled.ts
+++ webpage/src/components/ImageSlider/ImageSlider.styled.ts
@@ -1,4 +1,11 @@
 import styled from 'styled-components';
 
 export const ImageSliderWrapper = styled.div`
+    animation: 12s App-galery-slide infinite;
+    position: absolute; top: 0; left: 0; z-index: -1
+`;
+
+export const ImageSliderContainer = styled.div`
+    height: 30em; position: relative;
+    background: white
 `;
--- webpage/src/components/ImageSlider/ImageSlider.tsx
+++ webpage/src/components/ImageSlider/ImageSlider.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { ImageSliderWrapper } from './ImageSlider.styled';
+import React, { useEffect, useState } from 'react';
+import { ImageSliderWrapper, ImageSliderContainer } from './ImageSlider.styled';
+import './ImageSlider.css'
 
 export interface Imagen {
    url: string,
@@ -10,25 +11,91 @@ interface ImageSliderProps {
    galeriaImagenes: Imagen[]
 }
 
-const getImages = (galeriaImagenes: Imagen[]): JSX.Element => {
-   const images: any = []
-   galeriaImagenes.forEach(imagen => {
-      images.push(
-      <ImageSliderWrapper>
-         <img src={imagen.url} id={imagen.name} alt={imagen.name} />
-      </ImageSliderWrapper>
-      )
-   }) 
-   return images
-}
-
-
 const ImageSlider = ({galeriaImagenes}: ImageSliderProps) => {
+   let slideIndex = 0
+   let timeoutId:any = null;
+   const [slides, setSlides] = useState<any>()
+   const [dots, setDots] = useState<any>()
+
+   useEffect(() => {
+      setSlides(document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>)
+      setDots(document.getElementsByClassName("dot") as HTMLCollectionOf<HTMLElement>)
+      if(slides)
+         showSlides();
+    },[slides]); 
+
+   const currentSlide = (index: number) => {
+      slideIndex = index
+      showSlides()
+   }
+   
+   const plusSlides = (step: number) =>{
+      if(step < 0) {
+         slideIndex -= 2
+         
+         if(slideIndex < 0) {
+            slideIndex = slides.length - 1
+         }
+      }
+      showSlides();
+   }
+   
+   const showSlides = () => {
+      for(let i = 0; i < slides.length; i++) {
+         slides[i].style.display = "none" ;
+         dots[i].classList.remove('active');
+      }
+      console.log(slideIndex)
+      slideIndex++
+      if(slideIndex > slides.length) {
+         slideIndex = 1
+      }
+      slides[slideIndex - 1].style.display = "block";
+      dots[slideIndex - 1].classList.add('active');
+      if(timeoutId) {
+         clearTimeout(timeoutId);
+      }
+      timeoutId = setTimeout(showSlides, 5000); // Change image every 5 seconds
+   }
+   
+   
+
+   const getImages = (galeriaImagenes: Imagen[]): JSX.Element => {
+      const images: any = []
+      let delay = 0
+      let skip = 4
+      
+      galeriaImagenes.forEach((imagen, index) => {
+         images.push(
+            <div className='mySlides' key={'slides'+index}>
+               <img src={imagen.url} id={imagen.name} alt={imagen.name} style={{
+                  animationDelay: delay+"s"
+                  }}/>
+            </div>
+         )
+         delay = delay+skip;
+      }) 
+      return images
+   }
+
+   const getDots = (galeriaImagenes:Imagen []) => {
+      const dots: any = []
+      galeriaImagenes.forEach((imagen, index) => {
+         dots.push(<span className="dot" key={"dot"+index} onClick={() => currentSlide(index)}></span>)
+      }) 
+      return dots
+   }
+
+
    return (
-      <div>
+      <div className='slider-container'>
          {getImages(galeriaImagenes)}
+         <div style={{textAlign: "center"}}>
+             {getDots(galeriaImagenes)}  
+         </div>
       </div>
    )
+   
 }
 
 export default ImageSlider;
