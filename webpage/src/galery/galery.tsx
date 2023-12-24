85d83582a8f816cfc0fca118da4323a2bd59cc59
Juan Pablo Gomez Martinez <juanpablo.gomezmartinez89@gmail.com>
2023-12-24 10:03:07 -0600

    galery

--- webpage/src/galery/galery.tsx
+++ webpage/src/galery/galery.tsx
@@ -5,8 +5,20 @@ import kujipy from '../assets/kujipy.jpg';
 import tiptsy from '../assets/tiptsy.jpg';
 import tiptsy2 from '../assets/tiptsy2.jpg';
 import grupo1 from '../assets/grupo1.jpg';
-import casaFrijol from '../assets/casa_frijol.jpg';
-import patioArenal from '../assets/patio_arenal.jpg';
+import banda2 from '../assets/banda2.jpg';
+import musicos from '../assets/musicos.jpg';
+import tiptsy3 from '../assets/tiptsy3.jpg';
+import tiptsy4 from '../assets/tiptsy4.jpg';
+import tiptsy5 from '../assets/tiptsy5.jpg';
+import tiptsy6 from '../assets/tiptsy6.jpg';
+import tiptsy7 from '../assets/tiptsy7.jpg';
+import musicos2 from '../assets/musicos2.jpg';
+import musicos3 from '../assets/musicos3.jpg';
+import musicos4 from '../assets/musicos4.jpg';
+import musicos5 from '../assets/musicos5.jpg';
+import musicos6 from '../assets/musicos6.jpg';
+import musicos7 from '../assets/musicos7.jpg';
+
 import './galery.css'
 
 enum SOUNDSTATUS {
@@ -32,14 +44,29 @@ const Galery = () =>{
             <div className='galeria'>
                 <h2>Galería Kujipy</h2>
                 <div className='galeria-row'>
-                    <div className='image-container'><img src={kujipy} id={"kujipyimg"} alt={"kujipy"} onClick={openFullscreen}></img></div>
-                    <div className='image-container'><img src={tiptsy} id={"tiptsy"} alt={"tiptsy"} onClick={openFullscreen}></img></div>
-                    <div className='image-container'><img src={tiptsy2} id={"tiptsy2"} alt={"tiptsy2"} onClick={openFullscreen}></img></div>
-                    <div className='image-container'><img src={grupo1} id={"grupo1"} alt={"grupo1"} onClick={openFullscreen}></img></div>
+                    <div className='image-container'><img className='img-vertical' src={kujipy} id={"kujipyimg"} alt={"kujipy"} onClick={openFullscreen}></img></div>
+                    <div className='image-container'><img className='img-horizontal' src={tiptsy} id={"tiptsy"} alt={"tiptsy"} onClick={openFullscreen}></img></div>
+                    <div className='image-container'><img className='img-vertical' src={tiptsy2} id={"tiptsy2"} alt={"tiptsy2"} onClick={openFullscreen}></img></div>
+                    <div className='image-container'><img className='img-horizontal' src={grupo1} id={"grupo1"} alt={"grupo1"} onClick={openFullscreen}></img></div>
+                </div>
+                
+                <div className='galeria-row'>
+                    <div className='image-container'><img className='img-vertical' src={tiptsy3} id={"tiptsy3"} alt={"tiptsy3"} onClick={openFullscreen}></img></div>
+                    <div className='image-container'><img className='img-horizontal' src={tiptsy4} id={"tiptsy4"} alt={"tiptsy4"} onClick={openFullscreen}></img></div>
+                    <div className='image-container'><img className='img-vertical' src={tiptsy5} id={"tiptsy5"} alt={"tiptsy5"} onClick={openFullscreen}></img></div>
+                    <div className='image-container'><img className='img-vertical' src={tiptsy6} id={"tiptsy6"} alt={"tiptsy6"} onClick={openFullscreen}></img></div>
                 </div>
                 <div className='galeria-row'>
-                    <div className='image-container'><img src={casaFrijol} id={"casaFrijol"} alt={"casaFrijol"} onClick={openFullscreen}></img></div>
-                    <div className='image-container'><img src={patioArenal} id={"patioArenal"} alt={"patioArenal"} onClick={openFullscreen}></img></div>
+                    <div className='image-container'><img className='img-vertical' src={musicos} id={"musicos"} alt={"musicos"} onClick={openFullscreen}></img></div>
+                    <div className='image-container'><img className='img-vertical' src={musicos2} id={"musicos2"} alt={"musicos2"} onClick={openFullscreen}></img></div>
+                    <div className='image-container'><img className='img-vertical' src={musicos3} id={"musicos3"} alt={"musicos3"} onClick={openFullscreen}></img></div>
+                    <div className='image-container'><img className='img-horizontal' src={musicos4} id={"musicos4"} alt={"musicos4"} onClick={openFullscreen}></img></div>
+                </div>
+                <div className='galeria-row'>
+                    <div className='image-container'><img className='img-vertical' src={musicos5} id={"musicos5"} alt={"musicos5"} onClick={openFullscreen}></img></div>
+                    <div className='image-container'><img className='img-vertical' src={musicos6} id={"musicos6"} alt={"musicos6"} onClick={openFullscreen}></img></div>
+                    <div className='image-container'><img className='img-vertical' src={musicos7} id={"musicos7"} alt={"musicos7"} onClick={openFullscreen}></img></div>
+                    <div className='image-container'><img className='img-vertical' src={tiptsy7} id={"tiptsy7"} alt={"tiptsy7"} onClick={openFullscreen}></img></div>
                 </div>
                 
                 
--- webpage/src/hiring/hiring.tsx
+++ webpage/src/hiring/hiring.tsx
@@ -9,13 +9,13 @@ const Hiring = () => {
                     Contacto
                 </h2>
                 <p>
-                    <b>Correo:</b> contrataciones@kujipy.com
+                    <b>Correo:</b> kujipy.suun@gmail.com
                 </p>
                 <p>
                     <b>Whatsapp:</b> +52 283 116 9385
                 </p>
                 <p>
-                    <b>Whatsapp 2:</b> +52 283 116 9385
+                    <b>Whatsapp 2:</b> +52 221 441 8649
                 </p>
                 <p>
                     <b>Facebook:</b> <a href="https://www.facebook.com/kujipy" target="_blank" >kujipy</a>
